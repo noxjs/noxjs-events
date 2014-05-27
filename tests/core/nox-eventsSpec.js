deef71d189cc39fdb1e97ebbda60ebccbf47fef2
Mauricio Soares <m.soares2603@gmail.com>
2014-05-27 16:09:20 -0300

    trying to write tests, but got some bugs in nox core

--- tests/core/nox-eventsSpec.js
+++ tests/core/nox-eventsSpec.js
@@ -1,5 +1,35 @@
-describe('description', function() {
-  it('should attach a click event to the element', function() {
-    expect(true).toBe(true);
-  });
+describe('Testing nox-events core', function() {
+  // var div;
+
+  // beforeEach(function() {
+  //   div = $('<div class="myClass">test</div>');
+  //   $(document.body).append(div);
+  // });
+
+  // afterEach(function() {
+  //   div.remove();
+  //   div = null;
+  // });
+
+  // it('should attach a click event to the element', function() {
+  //   Nox('App.Test', 'events', function(Test, vent) {
+  //     Test.fn.initialize = function() {
+  //       vent('.myClass').on('click', this.clickHandler);
+  //     };
+
+  //     Test.fn.clickHandler = function() {
+  //       return true;
+  //     }
+  //   });
+
+  //   var instanceOfApp = new App.Test();
+  //   console.log(instanceOfApp.prototype);
+  //   spyOn(instanceOfApp.__proto__, 'clickHandler');
+
+  //   instanceOfApp.initialize();
+  //   console.log($('.myClass')[0].tagName);
+  //   $('.myClass').trigger('click');
+
+  //   expect(instanceOfApp.__proto__.clickHandler).toHaveBeenCalled();
+  // });
 });
