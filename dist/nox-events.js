69f0f7ae7c31f1cf8dc968eae26545c677c8ad34
Mauricio Soares <m.soares2603@gmail.com>
2014-05-27 13:56:58 -0300

    implement sniffing

--- dist/nox-events.js
+++ dist/nox-events.js
@@ -22,8 +22,57 @@ Nox.module('events', function(box) {
     this.elements = document.querySelectorAll(el);
   };
 
-  Events.prototype.on = function(action, callback) {
-    console.log(this.elements);
-    console.log(action, callback);
+  Events.fn = Events.prototype;
+
+  Events.fn.on = function(action, callback) {
+    this.callback = callback;
   };
+
+  Events.fn.unbind = function(action, callback) {
+    this.callback = callback;
+  };
+
+
+  /**
+  * Sniffing
+  * this is a pattern for detecting compability in browsers
+  * instead checking if the event is compatible with the
+  * browser every time i try to use it, i just check it once
+  * and attach it to a variable
+  */
+  Events.utils = {
+    addListener: null,
+    removeListener: null
+  };
+
+  if(typeof window.addEventListener === 'function') {
+    // actually browsers
+    Events.utils.addListener = function(el, type, fn) {
+      el.addEventListener(type, fn, false);
+    };
+
+    Events.utils.removeListener = function(el, type, fn) {
+      el.removeListener(type, fn, false);
+    };
+
+  } else if(typeof document.attachEvent === 'function') {
+    // old IE
+    Events.utils.addListener = function(el, type, fn) {
+      el.attachEvent('on' + type, fn);
+    };
+
+    Events.utils.removeListener = function(el, type, fn) {
+      el.detachEvent('on' + type, fn);
+    };
+
+  } else {
+    // damn that's old
+    Events.utils.addListener = function(el, type, fn) {
+      el['on' + type] = fn;
+    };
+
+    Events.utils.removeListener = function(el, type, fn) {
+      el['on' + type] = null;
+    };
+  }
 });
