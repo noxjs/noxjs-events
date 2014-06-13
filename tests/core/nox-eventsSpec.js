6d62013a2119dd608588884a6af5b5e76c672bbf
Mauricio Soares <m.soares2603@gmail.com>
2014-06-13 10:50:02 -0300

    fix test

--- tests/core/nox-eventsSpec.js
+++ tests/core/nox-eventsSpec.js
@@ -1,8 +1,10 @@
 describe('Testing nox-events core', function() {
-  it('should the vent function into the Nox contructor', function() {
+  it('should define the vent function into the Nox contructor', function() {
 
     Nox('App.Test', 'events', function(Test, vent) {
       expect(vent).not.toBeUndefined();
     });
+
+    new App.Test();
   });
 });
